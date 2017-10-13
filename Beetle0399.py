4177199916b2a92d3467326e3957a2d102b15e98
NickLatkovich <nicklatkovich@gmail.com>
2017-10-13 23:03:58 +0300

    Beetle

--- Beetle0399.py
+++ Beetle0399.py
@@ -1,24 +1,103 @@
+import queue
 
-def init2DArray(height, width, default_value):
+
+class Point:
+    def __init__(self, _y, _x):
+        self.y = _y
+        self.x = _x
+
+    def __add__(self, other):
+        return Point(self.y + other.y, self.x + other.x)
+
+    def __getitem__(self, item):
+        return item[self.y][self.x]
+
+    def __setitem__(self, key, value):
+        key[self.y][self.x] = value
+
+    def __eq__(self, other):
+        if type(other) != Point:
+            return False
+        else:
+            return self.y == other.y and self.x == other.x
+
+    def __iadd__(self, other):
+        return self + other
+
+
+dy = [1, 0, -1, 0]
+dx = [0, 1, 0, -1]
+dPoints = []
+for _dy, _dx in zip(dy, dx):
+    dPoints.append(Point(_dy, _dx))
+
+
+def hasWay(_walls, _from, to):
+    usedCells = init2DArray(len(_walls), len(_walls[0]) if len(_walls) > 0 else 0, False)
+    q = queue.Queue()
+    q.put(_from)
+    while not to[usedCells] and not q.empty():
+        cell = q.get()
+        for dPoint in dPoints:
+            nearCell = cell + dPoint
+            if not nearCell[_walls] and not nearCell[usedCells]:
+                nearCell[usedCells] = True
+                q.put(nearCell)
+    return to[usedCells]
+
+
+def init2DArray(height, width, default_value=None):
     result = []
-    for y in range(height):
+    for _y in range(height):
         result.append([])
-        for x in range(width):
-            result[y].append(default_value)
+        for _x in range(width):
+            result[_y].append(default_value)
     return result
 
-def hasWay(map: list, _from, to):
-    usedCells = init2DArray(len(map), len(map[0]) if len(map) > 0 else 0, False)
+
+def beetle():
+    with open('INPUT.TXT', 'r') as f:
+        h, w = map(int, f.readline().split())
+        _walls = init2DArray(h, w, False)
+        for y in range(h):
+            row = f.readline()
+            for x in range(w):
+                if row[x] == '@':
+                    _walls[y][x] = True
+    with open('OUTPUT.TXT', 'w') as f:
+        prevDirection = None
+        position = Point(1, 1)
+        finishPosition = Point(h - 2, w - 2)
+        if hasWay(_walls, position, finishPosition):
+            obstruction = init2DArray(h, w, 0)
+            result = 0
+            while position != finishPosition:
+                position[obstruction] += 1
+                minStepWeight = float('inf')
+                minStepWeightCount = -1
+                minStepDirection = None
+                for dPoint in dPoints:
+                    nextPosition = position + dPoint
+                    if nextPosition[_walls]:
+                        continue
+                    weight = nextPosition[obstruction]
+                    if weight < minStepWeight:
+                        minStepWeight = weight
+                        minStepWeightCount = 0
+                        minStepDirection = dPoint
+                    if weight == minStepWeight:
+                        minStepWeightCount += 1
+                        if dPoint == prevDirection:
+                            minStepDirection = dPoint
+                prevDirection = minStepDirection
+                position += minStepDirection
+                result += 1
+                # if result > 2000000:
+                #     f.write('-1')
+                #     return
+            f.write(str(result))
+        else:
+            f.write('-1')
 
 
-arr = []r
-with open('INPUT.TXT', 'r') as f:
-    h, w = map(int, f.read().split())
-    for y in range(h):
-        row = f.read()
-        arr.append([])
-        for x in range(w):
-            arr[y].append(True if row[x] == '@' else False)
-x = 1
-y = 1
-with open('OUTPUT.TXT', 'w') as f:
+beetle()
